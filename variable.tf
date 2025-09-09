08d686e39bf563eeaa9320feef34d88456330fbe
Abdulaziz920 <149649996+Abdulaziz920@users.noreply.github.com>
2025-09-09 16:43:03 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "filename" {
+  default =  "animals.txt"
+  type = string
+ 
+}
+variable "content" {
+  default = "We are here to test thats it ."
+  type = string
+}
+variable "prefix" {
+  default = "MR"
+  type = string
+}
+variable "length" {
+  default = 2
+  type = number
+}
