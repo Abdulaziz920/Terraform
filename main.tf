44482001ff27d04eeea618b503ed193fd2e0d534
Abdulaziz920 <149649996+Abdulaziz920@users.noreply.github.com>
2025-09-09 16:55:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ resource "local_file" "my_pet" {
 filename = var.filename
 content = var.content
 }
+resource "random_pet" "My-pet" {
+  prefix    = var.prefix
+  separator = "."
+  length    = var.length
+
+}
